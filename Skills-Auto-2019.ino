dd4b9189568e1e40dbcf8b8cc6f41f2f570cbf1e
Haashim Rehan <haashimr@hotmail.com>
2018-11-29 14:25:31 -0500

    Initial Commit

---
+++ Skills-Auto-2019.ino
@@ -0,0 +1,30 @@
+// Clockwise and counter-clockwise definitions.
+// Depending on how you wired your motors, you may need to swap.
+#define FORWARD  0
+#define REVERSE 1
+
+// Motor definitions to make life easier:
+#define MOTOR_A 0
+#define MOTOR_B 1
+
+// Pin Assignments //
+//Default pins:
+#define DIRA 12 // Direction control for motor A
+#define PWMA 3  // PWM control (speed) for motor A
+#define DIRB 13 // Direction control for motor B
+#define PWMB 11 // PWM control (speed) for motor B
+
+int turnSpeed = 120;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  setupArdumoto(); // Set all pins as outputs
+}
+
+void loop() {
+
+}
+
+
